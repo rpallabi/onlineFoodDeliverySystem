912f08c03b4c24d7f98428e7b67426f99bd786f6
Rajoshree342 <79593525+Rajoshree342@users.noreply.github.com>
2023-10-08 00:08:37 +0530

    Add files via upload

---
+++ src/main/java/com/Food/repository/FoodRepository.java
@@ -0,0 +1,9 @@
+package com.Food.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Food.entities.Food;
+
+public interface FoodRepository extends JpaRepository<Food, Integer>{
+
+}
