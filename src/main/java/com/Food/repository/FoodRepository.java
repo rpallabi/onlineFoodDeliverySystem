008f884408025da510afa4d44c06e651381b6f35
rpallabi <147096765+rpallabi@users.noreply.github.com>
2023-10-09 01:50:48 +0530

    Update FoodRepository.java to resolve merge conflicts

--- src/main/java/com/Food/repository/FoodRepository.java
+++ src/main/java/com/Food/repository/FoodRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.Food.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,14 +9,3 @@ import com.Food.entities.Food;
 public interface FoodRepository extends JpaRepository<Food, Integer>{
 
 }
-=======
-package com.Food.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Food.entities.Food;
-
-public interface FoodRepository extends JpaRepository<Food, Integer>{
-
-}
->>>>>>> 912f08c03b4c24d7f98428e7b67426f99bd786f6
